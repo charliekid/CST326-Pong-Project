dcb3c20417b6aca06ee8549fea66a8c5aafd91c2
Charlie Nguyen <36280021+charliekid@users.noreply.github.com>
2020-09-04 20:20:25 -0700

    initial

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float amplify = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            AddForce();
+        }
+        
+       
+    }
+
+    private void AddForce()
+    {
+        // goes straight down the x axis
+        rb.AddForce(new Vector3(1, 0, 0 * amplify));
+    }
+ }
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+     public float amplify = 1;
+// Start is called before the first frame update
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // we are translating every frame
+        // this occurs when the player motion down on one of the axis
+
+        //if (Mathf.Abs(transform.position.z) < 5.5) try to figure out how to block the ends from leaving
+        //{
+        transform.Translate(new Vector3(0, 0, Input.GetAxis("Vertical")) * Time.deltaTime * amplify);
+        transform.Translate(new Vector3(0, 0, Input.GetAxis("Right Paddle")) * Time.deltaTime * amplify);
+
+
+
+
+
+        //}
+
+    }
+}
